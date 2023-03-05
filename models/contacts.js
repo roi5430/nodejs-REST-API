c9da6a8176bcdcb3a3358218360d42aaceca0146
kseniia <104863533+roi5430@users.noreply.github.com>
2023-03-05 20:21:24 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,7 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
-// const contactsPath = path.join(__dirname, contacts.json);
 const contactsPath = path.resolve("./models/contacts.json");
 
 const listContacts = async () => {
