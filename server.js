4294deeb577edc877222f41ca7c6b09c4bea073d
kseniia <104863533+roi5430@users.noreply.github.com>
2023-03-07 13:25:02 +0200

    added mongoose

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const BD_HOST =
+  "mongodb+srv://oksanarobeiko:jrcfyf99@cluster0.gbbcn6s.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(BD_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
