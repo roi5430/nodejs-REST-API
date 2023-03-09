93da652e35a5d55094cca7dfc5abf9c7a4004fa1
kseniia <104863533+roi5430@users.noreply.github.com>
2023-03-09 17:48:11 +0200

    added valid, status

---
+++ helpers/index.js
@@ -0,0 +1,5 @@
+const HttpError = require("./HttpError");
+const ctrlWrapper = require("./ctrlWrapper");
+const mongooseError = require("./mongooseError");
+
+module.exports = { HttpError, ctrlWrapper, mongooseError };
---
+++ models/contact.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { mongooseError } = require("../helpers");
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+    required: [true, "Set phone for contact"],
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Contact = model("contact", contactSchema);
+
+const JoiSchema = Joi.object({
+  name: Joi.string().min(2).required(),
+  favorite: Joi.boolean(),
+  phone: Joi.string().required(),
+  email: Joi.string().required().email({
+    minDomainSegments: 2,
+  }),
+});
+
+const updateContactSchema = Joi.object({
+  name: Joi.string().min(2),
+  favorite: Joi.boolean(),
+  phone: Joi.string(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+  }),
+});
+
+const favoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+contactSchema.post("save", mongooseError);
+
+module.exports = { Contact, JoiSchema, updateContactSchema, favoriteSchema };
